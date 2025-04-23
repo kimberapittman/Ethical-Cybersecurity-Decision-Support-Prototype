f6011188261ed13e44efb6fecb2b7a3b03a76344
kimberapittman <kimberapittman@gmail.com>
2025-04-22 22:35:29 -0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ from logic.ethics import evaluate_ethics
 from logic.nist import map_nist_functions
 
 # ✅ CHDS logo hosted on Imgur
-st.image("https://i.imgur.com/1R7nuZ2.jpeg", width=700)
+st.image("https://i.imgur.com/1R7nuZ2.jpeg", width=900)
 
 # Title and intro
 st.title("🛡️ Ethical Cybersecurity Decision Tool")
