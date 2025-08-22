00d04844b4b4f9a948f6de6b5efb826aa55e07c5
kimberapittman <kimberapittman@gmail.com>
2025-08-21 22:19:58 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -201,7 +201,8 @@ preset_data = {
 
 # ---------- Intro ----------
 st.title("🛡️ Municipal Ethical Cyber Decision-Support (Prototype)")
-st.caption("Integrates NIST CSF 2.0 + Principlist ethics, within municipal institutional and governance constraints.")
+st.caption("Because What’s secure isn’t always what’s right.
+A structured tool for making ethical, justifiable decisions in municipal cybersecurity..")
 
 with st.expander("About this prototype"):
     st.markdown(
