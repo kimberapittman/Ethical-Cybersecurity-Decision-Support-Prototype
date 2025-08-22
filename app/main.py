02d0b2a6b35bfaa2e5dc8fe0e59e55defdaec09f
kimberapittman <kimberapittman@gmail.com>
2025-08-21 22:27:07 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -201,7 +201,7 @@ preset_data = {
 
 # ---------- Intro ----------
 st.title("🛡️ Municipal Ethical Cyber Decision-Support (Prototype)")
-st.caption(st.caption("Because What's secure isn't always what's right.")
+st.markdown("**Because what's secure isn't always what's right.**")
 st.caption("A structured tool for making ethical, justifiable decisions in municipal cybersecurity.")
 
 with st.expander("About this prototype"):
