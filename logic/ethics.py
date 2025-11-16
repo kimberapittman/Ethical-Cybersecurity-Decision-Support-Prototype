bb0a9a2df66027741982503f09638e98e14be800
kimberapittman <kimberapittman@gmail.com>
2025-11-15 23:29:09 -0600

    Delete logic/ethics.py

--- logic/ethics.py
+++
@@ -1,9 +0,0 @@
-
-def evaluate_ethics(beneficence, non_maleficence, autonomy, justice, explicability):
-    return f"""
-- **Beneficence:** {beneficence}
-- **Non-maleficence:** {non_maleficence}
-- **Autonomy:** {autonomy}
-- **Justice:** {justice}
-- **Explicability:** {explicability}
-"""
